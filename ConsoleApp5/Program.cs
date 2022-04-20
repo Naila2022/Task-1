2695f96f5e2c4626e26f7b41cbb8cd1b79588479
Sevil Alovsadli <nailaib@code.edu.az>
2022-04-20 15:40:24 +0400

    Task1

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bU tasKI metHodla yAzmAliSiz");
+            string str = Console.ReadLine();
+            string str1 = "Bu Taski Methodla Yazmalisiz";
+            foreach (char c in str1)
+            {
+                if (char.IsUpper(c))
+                    str1 += Char.ToLower(c);
+                
+                    
+            }
+            Console.WriteLine(str1);
+
+        }
+    }
+}
